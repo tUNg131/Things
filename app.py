ccecd0bb7dd0997cae1b96835e37a7a61e2d9117
tUNg131 <halethuy.hlt@gmail.com>
2020-05-13 19:47:11 +0700

    Add signup route

--- app.py
+++ app.py
@@ -5,12 +5,16 @@ from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired, EqualTo, Length, Email
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import or_
 
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'application'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rvgrjnphlwzvjd:fefede7bb2b893a43460c53ac701842ac969b1066d908d9b8f1639ec6e808d54@ec2-79-125-26-232.eu-west-1.compute.amazonaws.com:5432/d8svuqp19g214c'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://LeXuanTung@localhost:5432/things1'
+# postgresql://[user[:password]@][netloc][:port][/dbname][?param1=value1&...
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rvgrjnphlwzvjd:fefede7bb2b893a43460c53ac701842ac969b1066d908d9b8f1639ec6e808d54@ec2-79-125-26-232.eu-west-1.compute.amazonaws.com:5432/d8svuqp19g214c'
 
 db = SQLAlchemy(app)
 
@@ -47,6 +51,14 @@ def load_user(user_id):
 def unauthorized():
     return redirect(url_for('login', next=request.endpoint))
 
+def redirect_dest(fallback):
+    dest = request.args.get('next')
+    try:
+        dest_url = url_for(dest)
+    except:
+        return redirect(fallback)
+    return redirect(dest_url)
+
 @app.route('/')
 def landing():
     return render_template('landing_page.html')
@@ -56,29 +68,39 @@ def landing():
 def index():
     return render_template('index.html')
 
-def redirect_dest(fallback):
-    dest = request.args.get('next')
-    try:
-        dest_url = url_for(dest)
-    except:
-        return redirect(fallback)
-    return redirect(dest_url)
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
+        user = User.query.filter(User.username == form.username.data,
+                                 User.password == form.password.data).first() # need to do the hash password
         if user:
-            if user.password == form.password.data: # need to do hash password
                 login_user(user)
                 return redirect_dest(fallback=url_for('index'))
-    return render_template('Login.html', form=form)
+        flash('Wrong username or password!')
+        return redirect(url_for('login'))
+    return render_template('login.html', form=form)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignUpForm()
-    return render_template('SignUp.html', form=form)
+    if form.validate_on_submit():
+        user = User.query.filter(or_(User.username == form.username.data,
+                                     User.password == form.password.data,
+                                     User.email == form.email.data)).first()
+        if not user:
+            user = User(username=form.username.data,
+                        password=form.password.data,
+                        email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+            # todo a error catch here if insertion is not successful
+            login_user(user)
+            return redirect_dest(fallback=url_for('index'))
+        else:
+            flash(f'User {user.username} has already existed!')
+            return redirect(url_for('signup'))
+    return render_template('signup.html', form=form)
 
 @app.route('/success')
 def success():
