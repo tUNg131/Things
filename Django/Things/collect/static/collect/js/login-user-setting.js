e20e688036c2e521c2db766f62505de0b00b4855
VuHung <vuhung1809pl@gmail.com>
2020-06-21 00:35:40 +0700

    Jquery style user setting

--- Django/Things/collect/static/collect/js/login-user-setting.js
+++ Django/Things/collect/static/collect/js/login-user-setting.js
@@ -1,15 +1,32 @@
-// $(document).load(() => {
-//     $('#acc-setting-box').addClass('hidden-class');
-// });
-
 $('#person-switcher').on('click', () => {
+    // Hide acc
+    $('#acc-setting-box').hide();
+    $('#acc-switcher').css({
+        backgroundColor: '#a1a6b3',
+        borderLeft: 'solid #6A6D75 1px',
+    });
+
+    // Show person
+    $('#person-switcher').css({
+        backgroundColor: '#FFFFFF',
+        border: 'none',
+    });
     $('#person-setting-box').show();
-    $('#acc-setting-box').hide()
 })
 
 $('#acc-switcher').on('click', () => {
+    // Hide person
     $('#person-setting-box').hide();
-    $('#acc-setting-box').show()
+    $('#person-switcher').css({
+        backgroundColor: '#a1a6b3',
+        borderRight: 'solid #6A6D75 1px',
+    });
+    // Show acc
+    $('#acc-switcher').css({
+        backgroundColor: '#FFFFFF',
+        border: 'none',
+    });
+    $('#acc-setting-box').show();
 })
 
 $('#person-switcher').click()
