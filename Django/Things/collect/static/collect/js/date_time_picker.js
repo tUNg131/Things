515dbda5e2630462b9de12305d95260a304dbf07
VuHung <vuhung1809pl@gmail.com>
2020-07-20 09:59:02 +0700

    time picker + day in week picker

---
+++ Django/Things/collect/static/collect/js/date_time_picker.js
@@ -0,0 +1,10 @@
+$('#time-picker').timepicker({
+    timeFormat: 'h:mm p',
+    interval: 30,
+    dynamic: false,
+    dropdown: true,
+    scrollbar: true,
+    minTime: '10',
+    maxTime: '9:00pm',
+    defaultTime: '7pm'
+})
