a8b9e78c3d146c947c1f5f84bd5e952ab218c395
tUNg131 <halethuy.hlt@gmail.com>
2020-08-17 11:32:56 +0700

    Add reset_password and change_password

--- Things/accounts/forms.py
+++ Things/accounts/forms.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordChangeForm, PasswordResetForm, SetPasswordForm
+from django.forms import ModelForm, BooleanField
 from .models import User
+from django.utils.translation import gettext_lazy as _
 
 class LoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
@@ -11,3 +13,30 @@ class RegisterForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ('full_name', 'email')
+
+class SettingsForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].disabled = True
+
+    class Meta:
+        model = User
+        fields = ['full_name', 'email', 'phone', 'address', 'detail_address', 'collecting_day', 'collecting_time']
+
+class PasswordChangeUserForm(PasswordChangeForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
+class PasswordResetUserForm(PasswordResetForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
+class SetPasswordUserForm(SetPasswordForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
--- Things/accounts/views.py
+++ Things/accounts/views.py
@@ -7,13 +7,20 @@ from django.contrib.auth.views import (
     PasswordResetDoneView,
     PasswordResetConfirmView,
     PasswordResetCompleteView,
-    )
+)
 from django.views.generic import UpdateView, CreateView
 from django.urls import reverse_lazy
 from django.core.exceptions import ImproperlyConfigured
 
 from .models import User
-from .forms import LoginForm, RegisterForm
+from .forms import (
+    LoginForm, 
+    RegisterForm, 
+    SettingsForm, 
+    PasswordChangeUserForm, 
+    PasswordResetUserForm, 
+    SetPasswordUserForm,
+)
 
 class LoginUser(LoginView):
     template_name = 'accounts/login.html'
@@ -22,18 +29,29 @@ class LoginUser(LoginView):
 class RegisterUser(CreateView):
     template_name = 'accounts/register.html'
     form_class = RegisterForm
-    success_url = reverse_lazy('login')
+    
+    def form_valid(self, form):
+        # send_email()
+        return super().form_valid(form)
 
 class Settings(UpdateView):
     template_name = 'accounts/settings.html'
     model = User
-    fields = ['full_name', 'email', 'phone', 'address', 'detail_address']
+    form_class = SettingsForm
 
 class LogoutUser(LogoutView):
     next_page = reverse_lazy('landing_page')
 
 class PasswordChangeUser(PasswordChangeView):
     success_url = reverse_lazy('password_change_done')
+    template_name = 'change_password_form.html'
+    form_class = PasswordChangeUserForm
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        print(context['form'].fields['old_password'].label)
+        print(context['form'].as_p())
+        return context
 
     def get_success_url(self):
         try:
@@ -44,16 +62,18 @@ class PasswordChangeUser(PasswordChangeView):
 
 
 class PasswordChangeDoneUser(PasswordChangeDoneView):
-    pass
-
+    template_name = 'password_change_success.html'
+    
 class PasswordResetUser(PasswordResetView):
-    pass
+    template_name = 'password_reset.html'
+    form_class = PasswordResetUserForm
 
 class PasswordResetDoneUser(PasswordResetDoneView):
-    pass
+    template_name = 'password_reset_letter.html'
 
 class PasswordResetConfirmUser(PasswordResetConfirmView):
-    pass
+    template_name = 'enter_new_password.html'
+    form_class = SetPasswordUserForm
 
 class PasswordResetCompletleUser(PasswordResetCompleteView):
-    pass
+    template_name = 'password_reset_success.html'
